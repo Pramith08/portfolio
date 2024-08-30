9f8c33d2403d595ab2334e92988c4028c24e1780
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-08-31 00:01:14 +0530

    initial experience and project page complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import About from './components/About'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="flixed top-0 -z-10 h-full w-full"></div>
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      </div>
       <div className="container mx-auto px-8">
         <Navbar />
-        <Home/>
+        <Home />
+        {/* <About /> */}
+        <Experience/>
+        <Projects/>
+        <Contact/>
       </div>
 
     </div>
