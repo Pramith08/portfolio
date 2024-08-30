4c46bacc9844a96f2d487e22c117ca23cac5adde
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-08-31 01:10:19 +0530

    initial resume download button added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
                         <motion.p initial={{x:-100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5, delay:1}} className='my-2 max-w-xl py-6 font-light tracking-tighter'>
                             The sun dipped below the horizon, casting a warm, golden hue across the landscape. The leaves rustled gently in the evening breeze, whispering secrets of the day gone by. In the distance, a river meandered lazily, its surface reflecting the fading light like a ribbon of molten gold. The air was filled with the scent of earth and pine, a reminder of the untamed beauty of nature. As twilight descended, the first stars began to twinkle in the sky, promising the serenity of a peaceful night ahead.
                         </motion.p>
+                        <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Download Resume</button>
                     </div>
                 </div>
             </div>
