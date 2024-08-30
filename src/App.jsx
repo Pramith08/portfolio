e0c1e299744214d66f7bd33e099a1b3a3f622543
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-08-31 00:46:53 +0530

    first bug fix in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import About from './components/About'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
