9d358282408634963fd3673a7608880e2c364f97
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-08-31 00:15:35 +0530

    initial contact page done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { CONTACT } from '../constants'
 
 const Contact = () => {
-  return (
-    <div className=""></div>
-  )
+    return (
+        <div className="border-b border-neutral-900 pb-4">
+            <h2 className='my-10 text-center text-4xl'>
+                Contact
+            </h2>
+            <div className="text-center tracking-tighter">
+                <p className="my-4">{CONTACT.address}</p>
+                <p className="my-4">{CONTACT.phoneNo}</p>
+                <a href="#" className="border-b my-10">{CONTACT.email}</a>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const Home = () => {
     return (
@@ -6,15 +7,15 @@ const Home = () => {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className=" pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                        <motion.h1 initial={{x:-100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5, delay:0}} className=" pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
                             Pramith Kiran
-                        </h1>
-                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                        </motion.h1>
+                        <motion.span initial={{x:-100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5, delay:0.5}} className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
                             Full Stack Developer
-                        </span>
-                        <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>
+                        </motion.span>
+                        <motion.p initial={{x:-100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5, delay:1}} className='my-2 max-w-xl py-6 font-light tracking-tighter'>
                             The sun dipped below the horizon, casting a warm, golden hue across the landscape. The leaves rustled gently in the evening breeze, whispering secrets of the day gone by. In the distance, a river meandered lazily, its surface reflecting the fading light like a ribbon of molten gold. The air was filled with the scent of earth and pine, a reminder of the untamed beauty of nature. As twilight descended, the first stars began to twinkle in the sky, promising the serenity of a peaceful night ahead.
-                        </p>
+                        </motion.p>
                     </div>
                 </div>
             </div>
