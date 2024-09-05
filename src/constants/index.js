bb75ba95292cfd44e8ff561cdc10a8edfc0490da
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-09-05 22:11:19 +0530

    project pic change

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import aeroEncrypt from "../assets/projects/aeroEncrypt.svg";
 import ezzySearch from "../assets/projects/ezzySearch.svg";
 import lungCancer from "../assets/projects/lungCancerDetectionSystem.svg";
-import portfolio from "../assets/projects/portfolio.svg";
+import discordEzzySearch from "../assets/projects/ezzySearchDiscord.svg";
 
 export const BIO = 'A Cyber Seucirty enthusiast with a deep focus and passion for crafting secure, impactful applications. My expertise in Python and Flutter allows me to build innovative solutions that address real-world challenges. With a solid foundation in Computer Science and hands-on experience in the field, I am equipped to tackle complex technical problems. Outside of work, I enjoy staying active through basketball and spending quality time with my dog. I invite you to explore my work and connect with me as I continue my journey in the tech world.'
 
@@ -52,7 +52,7 @@ export const PROJECTS = [
   },
   {
     title: "Ezzy Search - Discord Bot",
-    images: [portfolio],
+    images: [discordEzzySearch],
     description:
       "Created a Discord bot for Ezzy Search, enabling users to seamlessly explore GitHub repositories within Discord. The bot integrates with the existing Python application and Streamlit interface, utilizing Selenium for automated web scraping to deliver real-time search results.",
     technologies: ["Python", "Streamlit", "Selenium", "Discord"],
