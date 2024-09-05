c09ff88ddd00075cdefa9176c2c3617af4ea30aa
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-09-05 12:59:28 +0530

    contact page icon changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,21 +9,21 @@ const Contact = () => {
     return (
         <section id='contact'>
             <div className="gap-10 items-center text-center justify-around pb-4 my-4">
-                <motion.h2 initial={{ x: -100, opacity: 0 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0 }} className='my-20 text-center text-4xl'>
+                <motion.h2 initial={{ x: -100, opacity: 0 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0 }} className='my-10 text-center text-4xl'>
                     Contact
-                </motion.h2>    
+                </motion.h2>
                 <motion.div initial={{ x: 100, opacity: 0 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0 }} className="lg:flex lg:flex-row lg:justify-around flex flex-col items-center gap-2 py-2 h-full tracking-tighter">
-                    <div className='flex gap-2 py-2 h-full'>
-                    <img src={locationicon} alt="Phone Icon" />
-                    <p className="my-4">{CONTACT.location}</p>
+                    <div className='flex gap-2 py-2 h-full items-center'>
+                        <img src={locationicon} alt="Phone Icon" className='h-12' />
+                        <p className="my-4">{CONTACT.location}</p>
                     </div>
-                    <div className='flex gap-2 py-2  h-full'>
-                    <img src={phoneicon} alt="Phone Icon" />
-                    <p className="my-4">{CONTACT.phoneNo}</p>
+                    <div className='flex gap-2 py-2  h-full items-center'>
+                        <img src={phoneicon} alt="Phone Icon" className='h-10' />
+                        <p className="my-4">{CONTACT.phoneNo}</p>
                     </div>
-                    <div className='flex gap-2 py-2  h-full'>
-                    <img src={emailicon} alt="Phone Icon" />
-                    <p className="my-4">{CONTACT.email}</p>
+                    <div className='flex gap-2 py-2  h-full items-center'>
+                        <img src={emailicon} alt="Phone Icon" className='h-10' />
+                        <p className="my-4">{CONTACT.email}</p>
                     </div>
                     {/* <a href="#" className="border-b my-10">{CONTACT.email}</a> */}
                 </motion.div>
