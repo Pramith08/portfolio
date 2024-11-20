5264c2e908688283e2973d2880c51b2700d580f2
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-11-20 19:16:27 +0530

    add motion to new carousel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,13 +10,13 @@ const Projects = () => {
                 <motion.h2 initial={{ x: -100, opacity: 0 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0 }} className='my-20 text-center text-4xl'>Projects</motion.h2>
                 <div>{PROJECTS.map((projects, index) => (
                     <div className="mb-12 flex flex-wrap lg:justify-around" key={index}>
-                        <div className="items-center justify-around flex lg:w-2/4">
+                        <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className="items-center justify-around flex lg:w-5/6">
                             <Carousel data = {projects.images} className="mb-6"/>
-                        </div>
+                        </motion.div>
                         {/* <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className="w-full lg:w-1/2">
                             <img src={projects.images[0]} alt={projects.title} className="mb-6 rounded w-full h-auto"/>
                         </motion.div> */}
-                        <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className="w-full mx-w-xl lg:w-1/4">
+                        <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className="w-full mx-w-xl lg:w-1/6">
                             <h6 className="mb-2 font-semibold">{projects.title}</h6>
                             <p className="mb-4 text-neutral-400">{projects.description}</p>
                             {projects.technologies.map((tech, index) => (
